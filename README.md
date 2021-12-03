b7b386e78fadf2311830d724ff1f1fec37554eb3
Kevin Olomu <kevin-olomu@web.de>
2021-12-03 08:34:53 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# README
+Exports Discord chat logs to a csv-file, then using that csv file for making name replacements.
+
+This project is based on [DiscordChatExporter](https://github.com/Tyrrrz/DiscordChatExporter).
+
+Therefore it also requires a `.NET Runtime` which has to be installed manually. 
+- [.NET Core v3.1 Runtime for **Windows x64**](https://dotnet.microsoft.com/download/dotnet-core/thank-you/runtime-desktop-3.1.0-windows-x64-installer)
+- [.NET Core v3.1 Runtime for **Windows x86**](https://dotnet.microsoft.com/download/dotnet-core/thank-you/runtime-desktop-3.1.0-windows-x86-installer)
+- [.NET Core v3.1 Runtime for **macOS x64**](https://dotnet.microsoft.com/download/dotnet-core/thank-you/runtime-3.1.0-macos-x64-installer)
+- [.NET Core v3.1 Runtime for **Linux**](https://docs.microsoft.com/en-us/dotnet/core/install/linux) (find your distro)
+
+
+You need to get your `Discord Token` and a `Channel ID` to use this tool.
+Rename `config.template.yaml` to `config.yaml` and insert there the Discord Token and Channel ID.
+This file is also responsible for the replacements pairs. 
+
+### How to get the Discord Token
+1. Open Discord
+2. Press Ctrl+Shift+I (⌥⌘I on macOS) to open developer tools
+3. Press Ctrl+Shift+M (⇧⌘M) to toggle device toolbar
+4. Navigate to the Application tab
+5. On the left, expand Local Storage and select https꞉//discord.com
+6. Type token into the Filter box
+7. If the token key does not appear, press Ctrl+R (⌘R) to reload
+8. Copy the value of the token key
+[Source](https://github.com/Tyrrrz/DiscordChatExporter/wiki/Obtaining-Token-and-Channel-IDs#how-to-get-a-user-token)
+
+or this video
+- https://www.youtube.com/watch?v=YEgFvgg7ZPI
+
+
+### How to get the channel id
+Receive the channel token in the browser version of discord.
+
+![Channel ID](./img/channel_id.png)
+
+### Run the App
+`python3 app.py`
---
+++ app.py
@@ -0,0 +1,60 @@
+import glob
+import pandas as pd
+import re
+import subprocess
+import os
+
+CHAT_PATH = "./discord_chats/"
+RESULT_PATH = "./out/"
+
+def read_config():
+    import yaml
+    with open("config.yaml", "r") as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+# searches in a directory for a file with a specified channel_id, returns dataframe
+def read_csv(channel_id=""):
+    if not channel_id:
+        channel_id = read_config()['channel_id']
+    found_files = glob.glob(f"{CHAT_PATH}*{channel_id}*.csv")
+    if(len(found_files) < 1):
+        raise ValueError(f"could not find file with channel id: {channel_id} at path: {CHAT_PATH}")
+    if(len(found_files) > 1):
+        raise ValueError(f"multiple files found with channel id: {channel_id} at path: {CHAT_PATH}")
+    return pd.read_csv(f'{found_files[0]}', sep=',')
+
+def replace_names(df):
+    df.drop(columns="AuthorID", inplace=True)
+    roles = read_config()['roles'] # dictionary of key: list
+    for role_item in roles.items():
+        role = role_item[0]
+        names_array = role_item[1]
+        for name in names_array:
+            re_replace_author_content = re.compile(re.escape(name), re.IGNORECASE)
+            re_remove_discord_tag = r"#\d{4}"
+            df['Content'].replace(to_replace=re_replace_author_content, value=f"{role}", inplace=True, regex=True)
+            df['Author'].replace(to_replace=re_replace_author_content, value=f"{role}", inplace=True, regex=True)
+            df['Author'].replace(to_replace=re_remove_discord_tag, value=f"", inplace=True, regex=True)
+    return df
+
+def main(): 
+    config = read_config()
+    channel_id = config['channel_id']
+    token = config['token']
+    current_dir = os.getcwd()
+
+    with open("logs/docker-output.log", "a") as output:
+        subprocess.call(
+            f"docker run --rm -v {current_dir}/discord_chats:/app/out tyrrrz/discordchatexporter:stable export -t {token} -c {channel_id} -f CSV", 
+            shell=True, 
+            stdout=output, 
+            stderr=output
+        )
+        df = read_csv()
+        df = replace_names(df)
+        df.to_csv(f'{RESULT_PATH}/{channel_id}.csv', index=False)
+
+main()
