338d253703d149d491aafcc8e3f2fb42b74c95f1
Kevin Olomu <kevin-olomu@web.de>
2021-12-17 16:19:41 +0100

    added --csv argument to specify a csv file.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import re
 import subprocess
 import os
+import argparse
+from pathlib import Path
 
 CHAT_PATH = "./discord_chats/"
 RESULT_PATH = "./out/"
@@ -16,15 +18,19 @@ def read_config():
             print(exc)
 
 # searches in a directory for a file with a specified channel_id, returns dataframe
-def read_csv(channel_id=""):
-    if not channel_id:
-        channel_id = read_config()['channel_id']
-    found_files = glob.glob(f"{CHAT_PATH}*{channel_id}*.csv")
-    if(len(found_files) < 1):
-        raise ValueError(f"could not find file with channel id: {channel_id} at path: {CHAT_PATH}")
-    if(len(found_files) > 1):
-        raise ValueError(f"multiple files found with channel id: {channel_id} at path: {CHAT_PATH}")
-    return pd.read_csv(f'{found_files[0]}', sep=',')
+def read_csv(channel_id="", path=""):
+    if not path:
+        if not channel_id:
+            channel_id = read_config()['channel_id']
+        found_files = glob.glob(f"{CHAT_PATH}*{channel_id}*.csv")
+        if(len(found_files) < 1):
+            raise ValueError(f"could not find file with channel id: {channel_id} at path: {CHAT_PATH}")
+        if(len(found_files) > 1):
+            raise ValueError(f"multiple files found with channel id: {channel_id} at path: {CHAT_PATH}")
+        return pd.read_csv(f'{found_files[0]}', sep=',')
+    else:
+        found_files = glob.glob(f"{path}")
+        return pd.read_csv(f'{found_files[0]}', sep=',')       
 
 def replace_names(df):
     df.drop(columns="AuthorID", inplace=True)
@@ -45,15 +51,26 @@ def main():
     channel_id = config['channel_id']
     token = config['token']
 
-    with open("logs/DiscordChatExporterCLI.log", "a") as output:
-        subprocess.call(
-            f"DiscordChatExporter\\DiscordChatExporter.Cli.exe export -o ./discord_chats -t {token} -c {channel_id} -f CSV",
-            shell=True,
-            stdout=output,
-            stderr=output
-        )
-        df = read_csv()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c',"--csv", type=str,help="if provided will replace for a `csv` file")
+    args = parser.parse_args()
+
+    if args.csv is not None:
+        csv_path = Path(args.csv)
+        filename = os.path.basename(csv_path)
+        df = read_csv(path=args.csv)
         df = replace_names(df)
-        df.to_csv(f'{RESULT_PATH}{channel_id}.csv', index=False)
+        df.to_csv(f'{RESULT_PATH}/{filename}', index=False)
+    else:
+        with open("logs/DiscordChatExporterCLI.log", "a") as output:
+            subprocess.call(
+                f"DiscordChatExporter\\DiscordChatExporter.Cli.exe export -o ./discord_chats -t {token} -c {channel_id} -f CSV",
+                shell=True,
+                stdout=output,
+                stderr=output
+            )
+            df = read_csv()
+            df = replace_names(df)
+            df.to_csv(f'{RESULT_PATH}{channel_id}.csv', index=False)
 
 main()
